ae370e8baf7f5b351d5e25e370b567bf35fd0bd0
Peter Krupa <erdengel.peter@outlook.de>
2017-01-10 20:57:49 +0100

    cleanup

--- StargateSuperRobot/RemoteControl.Wpf/Globals.cs
+++ StargateSuperRobot/RemoteControl.Wpf/Globals.cs
@@ -11,7 +11,5 @@ namespace RemoteControl.Wpf
         public const string DEVICE_ID = "f56bccc1-4075-d40f-7d0d-34c16a1411e0";
         public const string CONNECTIONSTRING_OWNER = "your shared access connection string";
         public const string WEBSOCKET_ENDPOINT = "ws://yoursite.azurewebsites.net/ws/receiver/";
-        ////public const string DEVICE_ID = "f56bccc1-4075-d40f-7d0d-34c16a1411e0";
-        ////public const string CONNECTIONSTRING_OWNER = "HostName=StarGateRoverHub2.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=V25rSSW1LPeKpeS6MZS8/KA4I1IHpXqJz9MROPxirwk=";
     }
 }
